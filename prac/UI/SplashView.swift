dc3a6d960601f3cefe63b9dc6c81877eb32eee47
nakisara01 <nakisara01@ajou.ac.kr>
2025-04-13 11:23:59 +0900

    Feat: Add to SplashScreen

---
+++ prac/UI/SplashView.swift
@@ -0,0 +1,37 @@
+//
+//  SplashView.swift
+//  prac
+//
+//  Created by 나현흠 on 4/12/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            MainPage() // 2초 후 이동할 메인 페이지
+        } else {
+            VStack {
+                Spacer()
+                Image("LaunchImage") // Assets에 있는 이미지
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200) // 원하는 사이즈로 조절
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white) // 배경색
+            .ignoresSafeArea()
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    withAnimation {
+                        isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
--- prac/pracApp.swift
+++ prac/pracApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct pracApp: App {
     var body: some Scene {
         WindowGroup {
-            MainPage()
+            SplashView()
         }
     }
 }
